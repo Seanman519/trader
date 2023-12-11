b0ddbc9eb32785cad6386641e0dfd3ac9e23c147
Seany519 <83579121+Seanman519@users.noreply.github.com>
2023-12-11 16:36:13 +0000

    changes - mess

---
+++ backend-bot/config.py
@@ -0,0 +1,18 @@
+import MetaTrader5 as mt5
+
+ACCOUNT = 25115284
+PASSWORD = "4zatlbqx"
+SERVER = "MetaQuotes-Demo"
+PAIRS = [
+    # "EURAUD",
+    # "EURUSD",
+    # "GBPUSD",
+    # "EURGBP",
+    # "AUDUSD",
+    # "USDJPY",
+    # "EURCAD",
+    "USDCAD"
+]
+TIMEZONE = "Asia/Brunei"
+LOT_SIZE = 0.1
+TIMEFRAME = mt5.TIMEFRAME_H1
